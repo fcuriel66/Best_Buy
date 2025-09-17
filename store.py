360b2f845ecd35308994ac1d2079c644b84ae0a5
Fernando Curiel <fcuriel66@mac.com>
2025-09-17 11:17:49 +0200

    Removed unnecessary comment lines

--- store.py
+++ store.py
@@ -40,15 +40,3 @@ class Store:
         for product, quantity in shopping_list:
             total_cost += product.buy(quantity)
         return total_cost
-
-# Test of class Store
-
-# product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-#                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-#                 products.Product("Google Pixel 7", price=500, quantity=250),
-#                ]
-#
-# best_buy = Store(product_list)
-# products = best_buy.get_all_products()
-# print(best_buy.get_total_quantity())
-# print(best_buy.order([(products[0], 1), (products[1], 2)]))
